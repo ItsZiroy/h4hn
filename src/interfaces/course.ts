0efe962e4b29a5c7dc9279fe6f5810cc244f8615
Yannik Hahn <yannik@h4hn.de>
2025-08-24 19:16:20 +0200

    feat: movile seachbar, qol changes

--- src/interfaces/course.ts
+++ src/interfaces/course.ts
@@ -1,5 +1,6 @@
 import type { Association } from "./association";
 import type File from "./file";
+import type { Icon } from "./icon";
 import type { Lecture } from "./lecture";
 import type { Semester } from "./semester";
 
@@ -8,6 +9,7 @@ export interface Course {
   name: string;
   slug: string;
   header: File;
+  links: Icon[];
   lectures: Lecture[];
   semester: Semester;
   association: Association;
